13180c330c870df5887837647074b985c1e15480
ChoiAhHyun <cah1113@naver.com>
2019-03-05 16:14:18 +0900

    학과 리스트, 스플래시 이미지 수정

--- app/src/main/java/com/sauce/inunion/SplashLogoActivity.java
+++ app/src/main/java/com/sauce/inunion/SplashLogoActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * Created by 123 on 2018-08-29.
@@ -20,6 +23,11 @@ public class SplashLogoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_logo);
+
+        ImageView imageView = (ImageView) findViewById(R.id.iv_splash);
+        Glide.with(getApplicationContext())
+                .load(R.drawable.splash_logo)
+                .into(imageView);
 
         new Handler().postDelayed(new Runnable(){
             @Override
